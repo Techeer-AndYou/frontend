8e92e0759f732cdf6ed31c621b874ff97950b078
TMInstaller <ajvldmlqjqclr@naver.com>
2023-09-19 13:15:04 +0900

    complete setting

---
+++ src/app/(user)/login/page.tsx
@@ -0,0 +1,3 @@
+export default function loginPage() {
+  return <main>로그인 페이지 입니다</main>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <main>여기는 메인 페이지 입니다</main>
+  return (
+    <main>
+      여기는 메인 페이지 입니다 <br />
+      <Link href='/login'>로그인 페이지로 이동</Link>
+      <Link href='/signup'>회원가입 페이지로 이동</Link>
+    </main>
+  )
 }
