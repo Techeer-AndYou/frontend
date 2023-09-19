70f8134be8b6fd8dc2a301e2ff37dd724182b1aa
TMInstaller <ajvldmlqjqclr@naver.com>
2023-09-19 13:33:35 +0900

    emotion 예시 작성 완료

--- src/app/(user)/login/page.tsx
+++ src/app/(user)/login/page.tsx
@@ -1,3 +1,22 @@
-export default function loginPage() {
-  return <main>로그인 페이지 입니다</main>
+'use client'
+import styled from '@emotion/styled'
+
+type ParagraphPropsType = {
+  hoverColor: string
 }
+
+const Paragraph = styled.p<ParagraphPropsType>`
+  font-size: 16px;
+  text-align: center;
+
+  &:hover {
+    color: ${(props) => props.hoverColor};
+  }
+`
+
+export default function loginPage() {
+  return <Paragraph hoverColor='red'>로그인 페이지 입니다</Paragraph>
+}
+
+// 이 페이지처럼 상호작용에 따른 동적인 스타일링이 적용되어있는 경우에는
+// 'use client'를 페이지 상단에 적어준다
