1ee9a5bee9b1dd78e13c2811610d53a850c2ffe2
TMInstaller <ajvldmlqjqclr@naver.com>
2023-10-05 23:49:20 +0900

    feat: SearchBar 추가

--- src/components/relation/Relation/Relation.tsx
+++ src/components/relation/Relation/Relation.tsx
@@ -3,29 +3,18 @@ import Image from 'next/image'
 import RelationGraph from './RelationGraph'
 import styled from '@emotion/styled'
 import Link from 'next/link'
+import { SearchBar } from '@/components/global/SearchBar'
 
-// 스크롤 바 숨기기 스타일, 현재 적용 안됨, 수정 필요
 const Container = styled.div`
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
-
-  @media (hover: none) and (pointer: coarse),
-    (-webkit-min-device-pixel-ratio: 1.5),
-    (min-resolution: 120dpi) {
-    &::-webkit-scrollbar {
-      display: none;
-    }
-  }
+  position: relative;
 `
 
 const StyledImage = styled.div`
+  display: flex; // flex로 변경하여 로고와 검색창이 수평으로 정렬되도록 합니다.
   position: absolute;
   top: 0;
   left: 0;
+
   & > div {
     display: unset !important; // next/image의 div 스타일 오버라이드
   }
@@ -43,6 +32,7 @@ export const Relation = () => {
         <Link href='/'>
           <Image src='/images/RememberPlusV2.png' width={150} height={56} alt='Service logo' />
         </Link>
+        <SearchBar />
       </StyledImage>
       <RelationGraph />
     </Container>
