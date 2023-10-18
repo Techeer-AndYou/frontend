e4f5e3b0a91e10dfa92da02c17354ce564186b06
TMInstaller <ajvldmlqjqclr@naver.com>
2023-10-19 03:10:23 +0900

    feat: 고정된 헤더 추가

--- src/app/(info)/main/page.tsx
+++ src/app/(info)/main/page.tsx
@@ -9,6 +9,40 @@ type ContainerProps = {
   isLoaded: boolean
 }
 
+const Header = styled.header`
+  position: fixed; // 고정 포지션으로 변경
+  width: 100vw; // 뷰포트 너비만큼 가로 길이 설정
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  height: 60px;
+  background-color: #fff;
+  box-shadow: 0px 1px 10px rgba(0, 0, 0, 0.1);
+`
+
+const Logo = styled.div`
+  font-weight: bold;
+  font-size: 20px;
+  color: black;
+`
+
+const Menu = styled.ul`
+  display: flex;
+  gap: 15px; // 메뉴 아이템들 사이의 간격
+  list-style: none; // 기본 리스트 스타일 제거
+`
+
+const MenuItem = styled.li`
+  font-size: 16px;
+  color: black;
+  cursor: pointer;
+
+  &:hover {
+    text-decoration: underline; // 마우스를 올렸을 때 밑줄이 생기도록 설정
+  }
+`
+
 const TopContainer = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
@@ -195,6 +229,14 @@ export default function MainPage() {
 
   return (
     <>
+      <Header>
+        <Logo>Remember Plus</Logo>
+        <Menu>
+          <MenuItem>Blog</MenuItem>
+          <MenuItem>마이페이지</MenuItem>
+          <MenuItem>로그아웃</MenuItem>
+        </Menu>
+      </Header>
       <TopContainer isLoaded={isPageLoaded}>
         <Image src='/images/find_friends.png' alt='친구찾기 카드' width={383} height={258} />
         <FindFriendsContainer isLoaded={isPageLoaded}>
