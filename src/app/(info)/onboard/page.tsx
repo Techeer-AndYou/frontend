3fcd45f0c44323f467b8aabe4b405a9fae42e21f
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-02 13:07:36 +0900

    comment: 작업 예정 내용 주석 추가

--- src/app/(info)/onboard/page.tsx
+++ src/app/(info)/onboard/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Onboard from '@/components/info/onboard'
 
+// 이곳에 와이어프레임 작성
 export default function onboardPage() {
   return (
     <main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import NightSky from '@/components/three/NightSky'
 import { useRouter } from 'next/navigation'
 
+// 이곳에 와이어프레임 작성
 const StartPage: React.FC = () => {
   const router = useRouter()
   // 여기는 시작 페이지 입니다.
