ab563bf6e98cdeff55c7a287b8e530624d0224e8
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-02 14:12:38 +0900

    feat: 컴포넌트화된 로고 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,20 @@
 'use client'
 import NightSky from '@/components/three/NightSky'
 import { useRouter } from 'next/navigation'
+import { Logo } from '@/components/global/Logo'
 
 // 이곳에 와이어프레임 작성
+// 와이어프레임 구조
+// 화면 중앙에 시작하기 버튼
+
 const StartPage: React.FC = () => {
   const router = useRouter()
   // 여기는 시작 페이지 입니다.
   return (
-    <main>
+    <>
+      <Logo animationType='fadeIn' animationDuration='2.4s' animationTimingFunction='ease' />
       <NightSky router={router} />
-    </main>
+    </>
   )
 }
 
