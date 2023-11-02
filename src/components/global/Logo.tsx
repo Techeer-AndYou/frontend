a995b57a894b7455f75fd8dbbf9bf633c3cc7a36
TMInstaller <ajvldmlqjqclr@naver.com>
2023-11-02 14:53:58 +0900

    refact: animation 분리

--- src/components/global/Logo.tsx
+++ src/components/global/Logo.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { animations } from '@/utils/animations'
 
 interface LogoProps {
   rememberColor?: string
@@ -6,32 +7,6 @@ interface LogoProps {
   animationType?: keyof typeof animations
   animationDuration?: string
   animationTimingFunction?: string
-}
-
-const animations = {
-  fadeIn: `
-    @keyframes fadeIn {
-      from {
-        opacity: 0;
-      }
-      to {
-        opacity: 1;
-      }
-    }
-  `,
-  slideIn: `
-    @keyframes slideIn {
-      from {
-        transform: translate(-50%, -50%) translateY(-20px);
-        opacity: 0;
-      }
-      to {
-        transform: translate(-50%, -50%) translateY(0);
-        opacity: 1;
-      }
-    }
-  `,
-  // 다른 애니메이션 효과 추가 가능
 }
 
 const LogoContainer = styled.div<LogoProps>`
